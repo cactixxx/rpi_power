127b18d0cf901094b00010be07215ee9f995c29b
cactixxx <eduplooy@gmail.com>
2022-09-29 20:36:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 cd /opt
-git clone https://github.com/larsks/gpio-watch.git
+sudo git clone https://github.com/larsks/gpio-watch.git
+sudo chown $(whoami) /opt/gpio-watch 
 cd gpio-watch
 make
 sudo make install
 
 sudo mkdir /opt/rpi_power
-cd /opt/rpi_power
-/usr/bin/wget -O - https://raw.githubusercontent.com/cactixx/rpi_power/rpi_power.sh
-chmod 755 /opt/rpi_power/rpi_power.sh
+sudo chown $(whoami) /opt/rpi_power
+/usr/bin/wget -O - https://raw.githubusercontent.com/cactixxx/rpi_power/main/rpi_power.sh > /opt/rpi_power/rpi_power.sh
+sudo chmod 744 /opt/rpi_power/rpi_power.sh
+sudo chown root /opt/rpi_power/rpi_power.sh
 crontab -l > root
 grep -qxF '@reboot /opt/rpi_power/rpi_power.sh' root || echo "@reboot /opt/rpi_power/rpi_power.sh" >> root
-crontab root
+sudo crontab root
 rm root
-/opt/rpi_power/rpi_power.sh

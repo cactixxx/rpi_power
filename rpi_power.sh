645450f21befa6285a6aed34eb075ed2ad4af779
cactixxx <eduplooy@gmail.com>
2022-09-29 19:50:10 +0200

    Create rpi_power.sh

---
+++ rpi_power.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OFF_BUTTON=20
+LATCH_OUT=21
+
+echo "#!/bin/sh" > /etc/gpio-scripts/$OFF_BUTTON
+echo "echo \"Off button pressed\"" >> /etc/gpio-scripts/$OFF_BUTTON
+echo "date \"+%F %H:%M:%S - Off button triggered\" >> /var/log/rpi_power.log" >> /etc/gpio-scripts/$OFF_BUTTON
+echo "/usr/sbin/init 0" >> /etc/gpio-scripts/$OFF_BUTTON
+chmod 777 /etc/gpio-scripts/$iOFF_BUTTON
+
+echo "$LATCH_OUT" > /sys/class/gpio/export	#output
+echo "out" > /sys/class/gpio/gpio$LATCH_OUT/direction
+echo "1" > /sys/class/gpio/gpio$LATCH_OUT/value
+date "+%F %H:%M:%S - On latch enabled" >> /var/log/rpi_power.log
+/opt/gpio-watch/gpio-watch $OFF_BUTTON:switch &

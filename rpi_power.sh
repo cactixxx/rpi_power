49dde045f8fe1d0e8f138649b17c369232724f22
cactixxx <eduplooy@gmail.com>
2022-09-29 22:24:25 +0200

    Update rpi_power.sh

--- rpi_power.sh
+++ rpi_power.sh
@@ -3,14 +3,14 @@
 OFF_BUTTON=20
 LATCH_OUT=21
 
-echo "#!/bin/sh" > /etc/gpio-scripts/$OFF_BUTTON
-echo "echo \"Off button pressed\"" >> /etc/gpio-scripts/$OFF_BUTTON
-echo "date \"+%F %H:%M:%S - Off button triggered\" >> /var/log/rpi_power.log" >> /etc/gpio-scripts/$OFF_BUTTON
-echo "/usr/sbin/init 0" >> /etc/gpio-scripts/$OFF_BUTTON
-chmod 777 /etc/gpio-scripts/$OFF_BUTTON
+sudo echo "#!/bin/sh" > /etc/gpio-scripts/$OFF_BUTTON
+sudo echo "echo \"Off button pressed\"" >> /etc/gpio-scripts/$OFF_BUTTON
+sudo echo "date \"+%F %H:%M:%S - Off button triggered\" >> /var/log/rpi_power.log" >> /etc/gpio-scripts/$OFF_BUTTON
+sudo echo "/usr/sbin/init 0" >> /etc/gpio-scripts/$OFF_BUTTON
+sudo chmod 777 /etc/gpio-scripts/$OFF_BUTTON
 
-echo "$LATCH_OUT" > /sys/class/gpio/export	#output
-echo "out" > /sys/class/gpio/gpio$LATCH_OUT/direction
-echo "1" > /sys/class/gpio/gpio$LATCH_OUT/value
-date "+%F %H:%M:%S - On latch enabled" >> /var/log/rpi_power.log
-/opt/gpio-watch/gpio-watch $OFF_BUTTON:switch &
+sudo echo "$LATCH_OUT" > /sys/class/gpio/export	#output
+sudo echo "out" > /sys/class/gpio/gpio$LATCH_OUT/direction
+sudo echo "1" > /sys/class/gpio/gpio$LATCH_OUT/value
+sudo date "+%F %H:%M:%S - On latch enabled" >> /var/log/rpi_power.log
+sudo /opt/gpio-watch/gpio-watch $OFF_BUTTON:switch &

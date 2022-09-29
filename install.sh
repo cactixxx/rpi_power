bd35e0d65d4f27bc375cc8ea48d4d2640e00f7c7
cactixxx <eduplooy@gmail.com>
2022-09-29 22:03:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo chown $(whoami) /opt/rpi_power
 /usr/bin/wget -O - https://raw.githubusercontent.com/cactixxx/rpi_power/main/rpi_power.sh > /opt/rpi_power/rpi_power.sh
 sudo chmod 744 /opt/rpi_power/rpi_power.sh
 sudo chown root /opt/rpi_power/rpi_power.sh
-crontab -l > root
+sudo crontab -l > root
 grep -qxF '@reboot /opt/rpi_power/rpi_power.sh' root || echo "@reboot /opt/rpi_power/rpi_power.sh" >> root
 sudo crontab root
 rm root

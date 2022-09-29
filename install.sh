a76e01d5e0a1c9a15589b7d5eee3e2876eadbe39
cactixxx <eduplooy@gmail.com>
2022-09-29 19:47:56 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /opt
+git clone https://github.com/larsks/gpio-watch.git
+cd gpio-watch
+make
+sudo make install
+
+sudo mkdir /opt/rpi_power
+cd /opt/rpi_power
+/usr/bin/wget -O - https://raw.githubusercontent.com/cactixx/rpi_power/rpi_power.sh
+chmod 755 /opt/rpi_power/rpi_power.sh
+crontab -l > root
+echo "@reboot /opt/rpi_power/rpi_power.sh" >> root
+crontab root
+rm root
+/opt/rpi_power/rpi_power.sh
